08f73ebcdca597de69bfdfd199fe12893d0828a6
Musab Sarmad <musabsarmadmir@gmail.com>
2025-07-22 20:07:34 +0500

    feat: new analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
+
 import type { Metadata } from 'next'
 import './globals.css'
+import { Analytics } from '@vercel/analytics/react'
 
 export const metadata: Metadata = {
   title: 'v0 App',
@@ -14,7 +16,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   )
 }
